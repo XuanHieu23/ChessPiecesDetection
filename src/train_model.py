0aed5931112c2db44a98ea62305519fc1a695c1b
TruongTuanHoang <hoangtt.23bi14173@usth.edu.vn>
2025-10-27 13:14:25 +0700

    train

--- src/train_model.py
+++ src/train_model.py
@@ -5,10 +5,10 @@ import os
 model = YOLO("yolov8n.pt")
 
 results = model.train(
-    data="data/Chess Pieces.yolov8-obb/data.yaml",  
+    data=".../data/Chess Pieces.yolov8-obb/data.yaml",  
     epochs=10,
     imgsz=640,
     batch=16,
     name="chess_yolo_model",
-    project="results"  # Creates 'results' folder in current directory
+    project="../results"  # Creates 'results' folder
 )
